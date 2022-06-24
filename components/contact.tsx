b59d9b44498a9eeae0e56457de9dcfb207abd4b2
jad-delgadillo <59192230+jad-delgadillo@users.noreply.github.com>
2022-06-23 23:42:12 -0500

    update

--- components/contact.tsx
+++ components/contact.tsx
@@ -3,10 +3,10 @@ import { IoIosAdd } from 'react-icons/io'
 
 export const Contact = () => {
   return (
-    <div className='flex flex-row justify-end items-center'>
-        <button className='flex flex-row tracking-widest uppercase text-white bg-black px-4 py-2 md:text-base text-xs items-center'>
-            <span>Contact us</span>
-            <IoIosAdd className='md:text-2xl text-lg'/>
+    <div className='flex md:justify-end justify-end mt-10 md:mt-0 items-center'>
+        <button className='flex flex-row tracking-widest uppercase text-white bg-black px-4 py-2 md:text-base text-xs items-center hover:animate-pulse'>
+            <span className='text-lg'>Contact us</span>
+            <IoIosAdd className='md:text-4xl text-4xl animate-spin'/>
             
         </button>
     </div>
