cfbd062d794bd06b1a3fba7fa73661bab92227cb
jad-delgadillo <59192230+jad-delgadillo@users.noreply.github.com>
2022-06-24 01:26:19 -0500

    update

---
+++ components/title.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Title = () => {
+  return (
+    <div>
+         <h1 className='flex font-black uppercase tracking-tight leading-none font-anton md:text-[16vw] text-[20vw]'>
+            Valiente <br/> Creative <br/> Studio
+        </h1>
+    </div>
+  )
+}
