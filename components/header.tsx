734bf60a8754377dc0f92423bd61ea2b4989acb5
jad-delgadillo <59192230+jad-delgadillo@users.noreply.github.com>
2022-06-24 11:51:00 -0500

    update

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div className='flex flex-row justify-between cursor-default'>
+    <div className='flex md:flex-row flex-col justify-between md:items-start items-center cursor-default'>
         <h1 className='flex font-black uppercase tracking-tight leading-none font-anton md:text-[16vw] text-[20vw]'>
             Valiente <br/> Creative <br/> Studio
         </h1>
